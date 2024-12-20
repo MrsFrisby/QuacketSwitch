99adf8bbc347310b820a6049e115d214da6eb5e7
Jessica Armitage <mrsjessicaarmitage@gmail.com>
2024-12-20 09:18:17 +0000

    Created Duck Object Script

---
+++ Assets/Scripts/DuckObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckObject : MonoBehaviour
+{
+    [SerializeField] private DucksSO duckObjectSO;
+
+    public DucksSO GetDucksSO()
+    {
+        return duckObjectSO;
+    }
+}
