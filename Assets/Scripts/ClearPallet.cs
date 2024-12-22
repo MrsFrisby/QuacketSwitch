5056dd808bce9359cec97de2e4cc02a56b7c30c1
Jessica Armitage <mrsjessicaarmitage@gmail.com>
2024-12-22 11:03:46 +0000

    Player can pick up/drop red duck onto Clear counter

---
+++ Assets/Scripts/ClearPallet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearPallet : BasePallet
+{
+    [SerializeField]
+    private DucksSO ducksSO;
+
+    //[SerializeField]
+    //private Transform palletTopPoint;
+
+    //private DuckObject duckObject;
+
+
+    public override void Interact(Player player)
+    {
+
+        if (!HasDuckObject())
+        {   //no duck already on pallet
+            if (player.HasDuckObject())
+            {
+                //player is carrying a duck
+                player.GetDuckObject().SetDuckObjectParent(this);
+            }
+            else
+            {
+                //can't do anything, no ducks!
+            }
+        }
+        else
+        {
+            //duck already on pallet
+            if (player.HasDuckObject())
+            {
+                //player already has a duck
+            }
+            else
+            {
+                //player empty handed
+                GetDuckObject().SetDuckObjectParent(player);
+            }
+        }
+    }
+
+    
+}
