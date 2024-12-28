c9a0a3a5d9fc063002e3724d8375a9d4b8013ab4
Jessica Armitage <mrsjessicaarmitage@gmail.com>
2024-12-28 14:34:44 +0000

    Added Shadergraph to delivery pallet

---
+++ Assets/Scripts/Pallets/DeliveryPallet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryPallet : BasePallet
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasDuckObject())
+        {
+            player.GetDuckObject().DestroySelf();
+        }
+
+
+    }
+}
