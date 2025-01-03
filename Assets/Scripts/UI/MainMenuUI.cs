2a1a4a78ff0679eb163b546781f6e69f4f25968b
Jessica Armitage <mrsjessicaarmitage@gmail.com>
2025-01-03 01:23:14 +0000

    Added start screen and scene loader

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button playButton;
+
+    [SerializeField]
+    private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {//lambda expression
+            //SceneManager.LoadScene(1);
+            Loader.Load(Loader.Scene.QuacketSwitch);
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+}
